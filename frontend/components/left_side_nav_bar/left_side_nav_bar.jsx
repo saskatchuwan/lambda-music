c05b93de43970b694d349663c04b9359eebafc09
Kathryn <kathryn.chu10@gmail.com>
2019-02-27 14:51:53 -0800

    Add rough frontend in-app scaffolding

--- frontend/components/left_side_nav_bar/left_side_nav_bar.jsx
+++ frontend/components/left_side_nav_bar/left_side_nav_bar.jsx
@@ -4,15 +4,44 @@ import { Link, NavLink } from 'react-router-dom';
 class LeftSideNavBar extends React.Component {
  
   render () {
+
+    const currentUser = this.props.currentUser;
+
+    let display;
+    if (currentUser) {
+      display = (
+        <div>
+          <h4>Welcome, {currentUser.username}!</h4>
+          <button onClick={this.props.logout}>Logout</button>
+        </div>
+      );
+
+    } else {
+      display = (
+        <div>
+          <Link to='/signup'>Sign up!</Link>
+          <br />
+          <Link to='/login'>Log in!</Link>
+        </div>
+      )
+
+    }
+
     return (
       <div className='left-side-nav-bar'>
+
         <h1>Nav</h1>
+        <br />
         <div className='side-bar-nav-links'>
           <NavLink to='/library/playlists'>Your Library</NavLink>
           <NavLink to='/browse/featured'>Home</NavLink>
           <NavLink to='/search'>Search</NavLink>
           <NavLink to='/playlist'>Temp playlist link</NavLink>
         </div>
+
+        {display}
+
+
       </div>
     );
   }
--- frontend/components/library/playlist/library_playlist_index.jsx
+++ frontend/components/library/playlist/library_playlist_index.jsx
@@ -4,34 +4,9 @@ import { Link } from 'react-router-dom';
 class LibraryPlayListIndex extends React.Component {
  
   render () {
-    const currentUser = this.props.currentUser;
-
-    let display;
-    if (currentUser) {
-      display = (
-        <div>
-          <h2>Welcome, {currentUser.username}!</h2>
-          <br />
-          <h1>library homepage placeholder</h1>
-          <br />
-          <button onClick={this.props.logout}>Logout</button>
-        </div>
-      );
-
-    } else {
-      display = (
-        <div>
-          <Link to='/signup'>Sign up!</Link>
-          <br />
-          <Link to='/login'>Log in!</Link>
-        </div>
-      )
-
-    }
-
     return (
       <div className='in-app-main'>
-          {display}
+          <h1>library/playlists placeholder</h1>
       </div>
     );
   }
