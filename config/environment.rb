d7eb1bd85d6a74d937683ef561981e53955ae82a
Kathryn <kathryn.chu10@gmail.com>
2019-02-26 16:16:48 -0800

    Fix visited a href styling

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# turn the keys you send up in your backend in camelCased format
+Jbuilder.key_format camelize: :lower
