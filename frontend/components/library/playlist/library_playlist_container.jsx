aedfd39d8c25938fb24c921434ed96471b38c984
Kathryn <kathryn.chu10@gmail.com>
2019-03-02 08:33:57 -0800

    Update library playlist component to render current user's saved playlistsgit add .

--- frontend/components/library/playlist/library_playlist_container.jsx
+++ frontend/components/library/playlist/library_playlist_container.jsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import LibraryPlayListIndex from './library_playlist_index';
 
-import { fetchSongs } from '../../../actions/song_actions';
+import { fetchUserPlaylists } from '../../../actions/playlist_actions';
 
 const mapStateToProps = (state, ownProps) => {
   //arrays
@@ -18,7 +18,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return ({
-
+    fetchUserPlaylists: (user_id) => dispatch(fetchUserPlaylists(user_id)),
   });
 };
 
--- frontend/components/library/playlist/library_playlist_index.jsx
+++ frontend/components/library/playlist/library_playlist_index.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class LibraryPlayListIndex extends React.Component {
+
+  componentDidMount () {
+    this.props.fetchUserPlaylists(this.props.currentUser.id);
+  }
  
   render () {
+    let { playlists } = this.props;
+
+    let playlistItems;
+
+    playlistItems = playlists.map(playlist => {
+      return (
+        <li key={playlist.id}>
+          {playlist.name}
+        </li>
+      )
+    });
+
     return (
       <div className='content-index-display-container'>
           <h1>Playlists curated by {this.props.currentUser.username}</h1>
           
           <div className='index-display-section'>
-
+            { playlistItems }
           </div>
       </div>
 
