db65c4958cfac5c3cd8f3597368caddbabe047a3
Kathryn <kathryn.chu10@gmail.com>
2019-03-07 11:26:04 -0800

    Add basic button overlayed on playlist tile; adjust html components so that the overall component can retain  click functionality to playlist show outside of play playlist button

--- frontend/components/library/playlist/library_playlist_index_item.jsx
+++ frontend/components/library/playlist/library_playlist_index_item.jsx
@@ -6,16 +6,19 @@ const LibraryPlaylistIndexItem = ({ playlist, playlistId, owner, fetchPlaybarPla
   
   return (
     <div className = 'tile-container'>
+        <div className='tile-attributes-container'>
 
-        <button onClick={() => fetchPlaybarPlaylist(playlistId)}>i'm a button</button>
+            <div className= 'tile'>
+              <a href={`/#/playlist/${playlist.id}`} onClick={(e) => e.stopPropagation()}>
+                <img src={`${playlist.coverUrl}`} />
+                <div className='tile-overlay'></div>
+              </a>
+            
+                <button className='play-content-button'
+                    onClick={() => fetchPlaybarPlaylist(playlistId)}>i'm a button</button>
+            </div>
 
-
-        <a href={`/#/playlist/${playlist.id}`}>
-          <div className= 'tile'>
-            <img src={`${playlist.coverUrl}`} />
-            <div className='tile-overlay'></div>
-          </div>
-        </a>
+        </div>
 
         <strong>
           <a href={`/#/playlist/${playlist.id}`}>
